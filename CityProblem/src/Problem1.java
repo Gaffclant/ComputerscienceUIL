27e3a823bdd95963093db1d533ade197937f982b
Ramenisneat <55544605+Ramenisneat@users.noreply.github.com>
2021-02-03 23:08:01 -0600

    Final code for the program

--- CityProblem/src/Problem1.java
+++ CityProblem/src/Problem1.java
@@ -1,4 +1,6 @@
 
+import com.sun.source.tree.WhileLoopTree;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -9,7 +11,7 @@ import java.util.stream.Collectors;
 
 public class Problem1 {
     public static void main(String[] args) throws IOException {
-        Graph g = MakeTestGraph();
+        //Graph g = MakeTestGraph();
         //System.out.println(g);
         //String s = g.find(g.nodes.get(0),g.nodes.get(5));
         //System.out.println(s);
@@ -20,6 +22,7 @@ public class Problem1 {
         char[][] grid = null;
 
         Graph graph = new Graph();
+        //converting the text into a grid
         for (int i =0;i<lines;i++) {
             String line = sc.next();
             if (grid == null)
@@ -29,7 +32,7 @@ public class Problem1 {
                 grid[i][j] = c;
                 if (c == 'o'){
                     graph.rootNode = new Node("o",new int[]{i,j});
-                    graph.nodes.add(graph.rootNode);
+                    //graph.nodes.add(graph.rootNode);
                 }
                 /**
                 switch (c) {
@@ -48,29 +51,82 @@ public class Problem1 {
                  **/
             }
         }
-        recurse(graph.rootNode.pos, grid, graph, null);
+
+        //main loop/algorithm
+        while (walls>0) {
+            graph.clear(); //clear the graph, we only save the root node but we remove it from the graph.nodes
+            recurse(graph.rootNode.pos, grid, graph, null); //Build the graph from city point
+            //System.out.println(graph);
+            if(graph.Edges.isEmpty()){     //If it is already safe, don't bother to run the next step
+                System.out.println("Success");
+                System.exit(1);
+                break;
+            }
+            for (Node E : graph.Edges){
+                String s = graph.find(E,graph.rootNode); //Find the shortest path from every edge node to the city
+                System.out.println(s);
+                String[] result = s.split(" => ");
+                for (String i : result){
+                    for (Node n : graph.nodes){
+                        if ((n.value).equals(i) && n != graph.rootNode)
+                            n.counter++; //Increase the counter of each node that is visited
+                    }
+                }
+
+                //System.out.println(result);
+
+            }
+            Node max = null;
+            for (Node n : graph.nodes){ //There may be a better way to find max using comparable, but I was too lazy.
+                if(max == null)
+                    max = n;
+                else
+                    max = n.counter>max.counter ? n : max;
+            }
+            grid[max.pos[0]][max.pos[1]]='x';
+            System.out.println(display(grid));
+
+        walls--;
+        }
+        graph.clear(); //Clears the graph again
+        recurse(graph.rootNode.pos, grid, graph, null);//final grid of city
         System.out.println(graph);
+        if (graph.Edges.isEmpty()) //If there is no edges in the final graph, then there is no possible path from city to edge. Therefore it is a success
+            System.out.println("success");
+        else
+            System.out.println("fail");
 
 
+    }
+    public static String display(char[][]grid){
+        String lineSeparator = System.lineSeparator();
+        StringBuilder sb = new StringBuilder();
 
+        for (char[] row : grid)
+            sb.append(Arrays.toString(row)).append(lineSeparator);
+
+        return sb.toString();
 
 
 
     }
 
+    //Recursive function that builds the graph
     public static void recurse(int[] pos, char[][] grid, Graph graph, Node prev){
 
         try {
             char val = grid[pos[0]][pos[1]];
             Node current = null;
 
-
-
             switch (val) {
                 case 'x':
                     return;
                 case 'o':
+                    if ((graph.nodes.contains(graph.rootNode)))
+                        return;
                     current = graph.rootNode;
+                    //System.out.println(graph);
+                    graph.nodes.add(graph.rootNode);
                     break;
                     /**
                     graph.rootNode = new Node("o", pos);
@@ -83,8 +139,10 @@ public class Problem1 {
                         if (Arrays.equals(n.pos,pos))
                             return;
                     }
-                    if (pos[0] == 0 || pos[0] == grid.length-1 || pos[1] == 0 || pos[1] == grid[0].length-1)
+                    if (pos[0] == 0 || pos[0] == grid.length-1 || pos[1] == 0 || pos[1] == grid[0].length-1) {
                         current = new Node(String.format("(E, %s, %s)", pos[0], pos[1]), pos);
+                        graph.Edges.add(current);
+                    }
                     else
                         current = new Node(String.format("(-, %s, %s)", pos[0], pos[1]), pos);
                     graph.nodes.add(current);
@@ -146,9 +204,21 @@ public class Problem1 {
 class Graph {
     public List<Node> nodes;
     public Node rootNode;
+    public List<Node> Edges;
 
     public Graph() {
         nodes = new ArrayList<Node>();
+        Edges = new ArrayList<Node>();
+
+
+    }
+
+    //clears the graph
+    public void clear(){
+
+        rootNode.connectedNodes.clear();
+        nodes.clear();
+        Edges.clear();
     }
 
     public String toString() {
@@ -160,6 +230,9 @@ class Graph {
     }
 
     public String find (Node A, Node B){
+        for (Node n : nodes){
+            n.path.clear();
+        }
         PriorityQueue <Node> queue = new PriorityQueue<>();
         String output = "";
         List<Node> visited = new ArrayList<>();
@@ -199,6 +272,7 @@ class Node implements Comparable<Node>{
     public ArrayList<Node> connectedNodes;
     public ArrayList<Node> path;
     public int[] pos = new int[2];
+    public int counter = 0;
 
     public Node(String value) {
         this.value = value;
